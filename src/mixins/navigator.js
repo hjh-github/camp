994d741565ed3fd56aed46a5ade6af95a71540bf
hjh <huangjh@kuan1.cn>
2019-12-09 17:50:35 +0800

    up

---
+++ src/mixins/navigator.js
@@ -0,0 +1,25 @@
+import wepy from 'wepy'
+export default class location extends wepy.mixin {
+    methods = {
+        navigator(url, type) {
+            if (type == 'none' || !type) {
+                return false
+            }
+            if (type == 'navigateTo') {
+                wepy.navigateTo({
+                    url
+                });
+            } else if (type == 'redirectTo') {
+                wepy.redirectTo({
+                    url
+                });
+            } else if (type == 'reLaunch') {
+                wepy.reLaunch({
+                    url
+                });
+            } else if (type == 'web') {
+                console.log('暂时先只打开本地路径')
+            }
+        }
+    };
+}
